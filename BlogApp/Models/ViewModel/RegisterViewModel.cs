58a53fa8e4e914b7c9fe4f35786d58d5208b0dc8
Rajesh Sharma (g) <rajesh.i.sharma@avanade.com>
2017-01-30 23:05:38 -0800

    confirm password field validation/ui

--- BlogApp/Models/ViewModel/RegisterViewModel.cs
+++ BlogApp/Models/ViewModel/RegisterViewModel.cs
@@ -23,5 +23,11 @@ namespace BlogApp.Models.ViewModel
         //[RegularExpression("([a-z]|[A-Z]|[0-9]|[\\W]){4}[a-zA-Z0-9\\W]{3,11}", ErrorMessage = "Invalid password format")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Retype Password")]
+        [MinLength(8, ErrorMessage = "Minimum length is 8 characters")]
+        [Compare(otherProperty:"Password",ErrorMessage ="Passwords Mismatch")]
+        //[RegularExpression("([a-z]|[A-Z]|[0-9]|[\\W]){4}[a-zA-Z0-9\\W]{3,11}", ErrorMessage = "Invalid password format")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
